534656853e4eb551c1d2f0e498cfcc61f18cd1c2
Bosun Egberinde <bosunski@gmail.com>
2024-03-13 17:51:23 +0100

    Update workflow

--- app/Updater/PharUpdater.php
+++ app/Updater/PharUpdater.php
@@ -2,12 +2,23 @@
 
 namespace App\Updater;
 
-class PharUpdater extends \Humbug\SelfUpdate\Updater
+use Humbug\SelfUpdate\Updater as BaseUpdater;
+
+class PharUpdater extends BaseUpdater
 {
     protected function validatePhar($phar): void
     {
-        dump('Validating Phar');
+        // The default validatePhar implmentation doesn't apply to our case so we override it
+    }
 
-        // Check if the Phar is running
+    protected function replacePhar(): void
+    {
+        /**
+         * We will get the current mode of the binary file and apply it to the new Phar file
+         * after we have moved it to the location of the old Phar file.
+         */
+        $currentMode = fileperms($this->getLocalPharFile());
+        rename($this->getTempPharFile(), $this->getLocalPharFile());
+        chmod($this->getLocalPharFile(), $currentMode);
     }
 }
