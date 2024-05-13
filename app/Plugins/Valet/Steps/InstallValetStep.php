21863a3e422f6130e229e5a60030bedf3149365e
Bosun Egberinde <bosunski@gmail.com>
2024-05-13 23:20:12 +0200

    WIP

--- app/Plugins/Valet/Steps/InstallValetStep.php
+++ app/Plugins/Valet/Steps/InstallValetStep.php
@@ -4,7 +4,6 @@ namespace App\Plugins\Valet\Steps;
 
 use App\Execution\Runner;
 use App\Plugin\Contracts\Step;
-use Exception;
 
 class InstallValetStep implements Step
 {
@@ -17,17 +16,11 @@ class InstallValetStep implements Step
         return 'Install Laravel Valet';
     }
 
-    /**
-     * @throws Exception
-     */
     public function run(Runner $runner): bool
     {
         return $runner->exec("$this->composerBinary global require laravel/valet");
     }
 
-    /**
-     * @throws Exception
-     */
     public function done(Runner $runner): bool
     {
         return is_file($this->valetBinary);
